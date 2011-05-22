7bc34323ec4656125c4f0413cf000255e6b6c7d6
Joris Nederpelt Lazarom <joris@nederpeltlazarom.nl>
2011-05-22 11:44:09 +0200

    Removed default rails home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
-end
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Tutorial_title"
+    if @title.nil?
+      base_title
+     else
+      "#{base_title} | #{@title}"
+     end
+    end    
+  end
